c8c0737dde29893112957f57e7c115b9400b3713
egydeveloper <www.michael.m.s.com@gmail.com>
2022-09-24 14:06:17 +0200

    frist edtion

---
+++ MyProject/Home.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MyProject
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+            
+        }
+        #region Header panel
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Login loginwindow = new Login();
+            loginwindow.Show();
+            this.Close();
+        }
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                WindowStyle = WindowStyle.SingleBorderWindow;
+                this.WindowState = WindowState.Maximized;
+                BorderThickness = new Thickness(this.WindowState == WindowState.Maximized ? 8 : 0);
+                WindowStyle = WindowStyle.None;
+            }
+            else if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                BorderThickness = new Thickness(0);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+
+        }
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+                this.WindowState = WindowState.Minimized;
+        }
+
+
+
+        #endregion
+
+       
+    }
+}
